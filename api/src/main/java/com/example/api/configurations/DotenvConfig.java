e6f3f6123712c2b0c81f5f95793675ab859a0f51
Ihavenojoy <tom.van.de.lest@gmail.com>
2025-05-07 14:35:25 +0200

    small update

--- api/src/main/java/com/example/api/configurations/DotenvConfig.java
+++ api/src/main/java/com/example/api/configurations/DotenvConfig.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.Objects;
+
 @Configuration
 public class DotenvConfig {
 
@@ -17,22 +19,22 @@ public class DotenvConfig {
                 .load();
 
         // Set system properties using values from .env file
-        System.setProperty("MINIO_ACCESS_KEY", dotenv.get("MINIO_ACCESS_KEY"));
-        System.setProperty("MINIO_SECRET_KEY", dotenv.get("MINIO_SECRET_KEY"));
-        System.setProperty("SSL_KEYSTORE_PASSWORD", dotenv.get("SSL_KEYSTORE_PASSWORD"));
+        System.setProperty("MINIO_ACCESS_KEY", Objects.requireNonNull(dotenv.get("MINIO_ACCESS_KEY")));
+        System.setProperty("MINIO_SECRET_KEY", Objects.requireNonNull(dotenv.get("MINIO_SECRET_KEY")));
+        System.setProperty("SSL_KEYSTORE_PASSWORD", Objects.requireNonNull(dotenv.get("SSL_KEYSTORE_PASSWORD")));
 
         // Set Dragonfly environment variables
-        System.setProperty("DRAGONFLY_PORT", dotenv.get("DRAGONFLY_PORT"));
+        System.setProperty("DRAGONFLY_PORT", Objects.requireNonNull(dotenv.get("DRAGONFLY_PORT")));
 
         // Set MySQL environment variables
-        System.setProperty("MYSQL_ROOT_PASSWORD", dotenv.get("MYSQL_ROOT_PASSWORD"));
-        System.setProperty("MYSQL_DATABASE", dotenv.get("MYSQL_DATABASE"));
-        System.setProperty("MYSQL_USER", dotenv.get("MYSQL_USER"));
-        System.setProperty("MYSQL_PASSWORD", dotenv.get("MYSQL_PASSWORD"));
-        System.setProperty("MYSQL_PORT", dotenv.get("MYSQL_PORT"));
+        System.setProperty("MYSQL_ROOT_PASSWORD", Objects.requireNonNull(dotenv.get("MYSQL_ROOT_PASSWORD")));
+        System.setProperty("MYSQL_DATABASE", Objects.requireNonNull(dotenv.get("MYSQL_DATABASE")));
+        System.setProperty("MYSQL_USER", Objects.requireNonNull(dotenv.get("MYSQL_USER")));
+        System.setProperty("MYSQL_PASSWORD", Objects.requireNonNull(dotenv.get("MYSQL_PASSWORD")));
+        System.setProperty("MYSQL_PORT", Objects.requireNonNull(dotenv.get("MYSQL_PORT")));
 
 
         // Set allowed origins for CORS
-        System.setProperty("ALLOWED_ORIGINS", dotenv.get("ALLOWED_ORIGINS"));
+        System.setProperty("ALLOWED_ORIGINS", Objects.requireNonNull(dotenv.get("ALLOWED_ORIGINS")));
     }
 }
